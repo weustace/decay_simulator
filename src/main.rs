4ec0e820dff2845bc67e2fd0887681ac20fc97a9
William Eustace <weustace@eustacesheen.me.uk>
2016-01-22 10:08:15 +0000

    Tidying up. Resolved compiler warnings.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,15 @@
 extern crate ansi_term;
 extern crate rand;
 
-
-use std::io;
 use std::thread::sleep_ms;
 use std::io::{Write,stdout};
 use rand::Rng;
-use std::vec;
 use ansi_term::Colour::Fixed;
-use std::string;
-
-
 
 struct Isotope {
     symbol :char,
     colour : u8,
     decay_prob : f64,
-
 }
 
 impl Isotope{
@@ -43,7 +36,7 @@ impl<'a> Atom<'a>{
     }
 
     fn step(&mut self) -> bool{//returns true if decayed
-        let sim_limit: i32 = 1000000000;
+        let sim_limit: i32 = 1_000_000_000;
         if self.current < self.chain.len() {
             let dp = self.chain[self.current].decay_prob;
             let rn = rand::thread_rng().gen_range(1,sim_limit);
@@ -58,7 +51,7 @@ impl<'a> Atom<'a>{
 }
 
 fn print_atoms(atoms:&Vec<Atom>){
-    stdout().write_all("\x1b[2J\x1b[1;1H".as_bytes());//based on CLI lib at https://github.com/fengsp/cli
+    stdout().write_all("\x1b[2J\x1b[1;1H".as_bytes()).unwrap();//based on CLI lib at https://github.com/fengsp/cli
     for atom in atoms {
         let current_isotope = &atom.chain[atom.current];
         print!("{}",Fixed(current_isotope.colour).paint(current_isotope.symbol.to_string()).to_string());
@@ -66,20 +59,24 @@ fn print_atoms(atoms:&Vec<Atom>){
 }
 
 fn main(){
-    let isotopes:Vec<_> = vec![Isotope::new('A',27,0.1f64),Isotope::new('B',28,0.2f64),Isotope::new('C',240,0f64)];
-    let mut atoms:Vec<_> = Vec::new();
-    for x in 0..8000 {
+    const N_ATOMS:usize= 8000;
+    const N_STEPS:usize = 500;
+
+    let isotopes = vec![Isotope::new('A',27,0.1f64),Isotope::new('B',28,0.02f64),Isotope ::new('C',240,0.001f64),Isotope::new('D',3,0f64)];
+    let mut atoms = Vec::new();
+    for _ in 0..N_ATOMS {
         atoms.push(Atom::new(&isotopes,0));
     }
-    for steps in 0..500 {
-        let mut decayCount = 0usize;
+    for _ in 0..N_STEPS {
+        let mut decay_count = 0usize;
         for i in 0..atoms.len() {
-            if atoms[i].step() {decayCount+=1;
-            }
+
+            if atoms[i].step() { decay_count+=1; }
+
         }
         print_atoms(&atoms);
         println!(" ");
-        println!("{} decayed",decayCount);
+        println!("{} decayed",decay_count);
         sleep_ms(300);
     }
 }
